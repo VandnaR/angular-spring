6802063c8ad9017cb1ec5814d105079f86593951
VandnaR <vandna.r42@wipro.com>
2020-07-06 22:12:17 +0530

    commit

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent implements OnInit {
+
+  books:any;
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+    let response=this.http.get("http://localhost:4200/findAllBooks");
+    response.subscribe((data)=>this.books=data)
+  }
+
+}
